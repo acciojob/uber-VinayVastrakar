631468fa336dfcce9aee4aff17b8f5d8aa6211d4
Vinay Vastrakar <Vinayvastrakar13@gmail.com>
2023-07-15 16:53:42 +0530

    3rd

--- src/main/java/com/driver/controllers/CustomerController.java
+++ src/main/java/com/driver/controllers/CustomerController.java
@@ -28,6 +28,7 @@ public class CustomerController {
 
 	@PostMapping("/bookTrip")
 	public ResponseEntity<Integer> bookTrip(@RequestParam Integer customerId, @RequestParam String fromLocation, @RequestParam String toLocation, @RequestParam Integer distanceInKm) throws Exception {
+		TripBooking book
 
 		return new ResponseEntity<>(bookedTrip.getTripBookingId(), HttpStatus.CREATED);
 	}
